5f0ecafc177101f0f9927fa38b496d0c0923cb97
andykr1k <69021102+andykr1k@users.noreply.github.com>
2022-07-03 11:16:58 -0700

    Updates all files in specified path

--- main.cpp
+++ main.cpp
@@ -8,8 +8,16 @@ using namespace std;
 int main() {
     string path = "/Users/andrew/Desktop/Projects/";
 
-    for (const auto & file : directory_iterator(path))
-        cout << file.path().filename() << endl;
+    for (const auto & file : directory_iterator(path)){
+        cout << file.path().filename().string() << " is being updated." << endl;
+        string string_path = file.path().filename().string();
+        string command = "cd /Users/andrew/Desktop/Projects/" + string_path + " &&  git pull";
+        int n = command.length();
+        char char_array[n + 1];
+        strcpy(char_array, command.c_str());
+        system(char_array);
+    }
+
 
     return EXIT_SUCCESS;
 }
